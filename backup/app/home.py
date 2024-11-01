6330e5309cdc4538e25c9a17cad492c9885655bd
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-11-01 00:08:14 -0300

    .

--- backup/app/home.py
+++ backup/app/home.py
@@ -1,53 +1,70 @@
 import flet as ft
 
-def home(page:ft.Page):
+class TelaHome:
+    def home(page:ft.Page):
 
-    #Pop-up de alerta. Ao clicar em fazer logoff, uma confirmação é chamada
-    alerta_logout=ft.AlertDialog(
-        modal=True,
-        bgcolor="#2A383E",
-        title=ft.Text("CONFIRMAR LOGOUT"),
-        content=ft.Text("Você realmente deseja fazer Logoff do aplicativo?"),
-        actions=[
-            ft.TextButton("Sim", on_click=lambda _:page.go("/")),
-            ft.TextButton("Não", on_click=lambda e:page.close(alerta_logout)),
-        ]
-    )
-
-    ###############################################################################
-    ###############################################################################
-
-    logo=ft.Image("app/assets\logo3.jpg")
-    icone_sair=ft.Container(
-        margin=15,
-        on_click=lambda e:page.open(alerta_logout),
-        content=ft.Image("app/assets\Ícone Sair.svg"),
-    )
-
-    appbar=ft.Container(
-        height=75,
-        bgcolor="#2A383E",
-        content=ft.Row(
-            [
-                logo,
-                icone_sair
-            ],
-            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
-        )
-    )
-
-#####################################################################################
-#####################################################################################
-
-    tela=ft.Container(
-        expand=True,
-        bgcolor="#D9D9D9",
-        content=ft.ResponsiveRow(
-            col={"xs":12,"sm":6,"md":4},
-            controls=[
-                appbar,
+        #Pop-up de alerta. Ao clicar em fazer logoff, uma confirmação é chamada
+        alerta_logout=ft.AlertDialog(
+            modal=True,
+            bgcolor="#2A383E",
+            title=ft.Text("CONFIRMAR LOGOUT"),
+            content=ft.Text("Você realmente deseja fazer Logoff do aplicativo?"),
+            actions=[
+                ft.TextButton("Sim", on_click=lambda _:page.go("/")),
+                ft.TextButton("Não", on_click=lambda e:page.close(alerta_logout)),
             ]
         )
-    )
 
-    return tela
+        ###############################################################################
+        ###############################################################################
+
+        logo=ft.Image("app/assets\logo3.jpg")
+        icone_sair=ft.Container(
+            margin=15,
+            on_click=lambda e:page.open(alerta_logout),
+            content=ft.Image("app/assets\Ícone Sair.svg"),
+        )
+
+        appbar=ft.Container(
+            height=75,
+            bgcolor="#2A383E",
+            content=ft.Row(
+                [
+                    logo,
+                    icone_sair
+                ],
+                alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+            )
+        )
+
+        sidebar=ft.Container(
+            width=160,
+            bgcolor="#2A383E",
+            content=ft.Column(
+                [
+                    ft.Text("Teste 1"),
+                    ft.Text("Teste 2"),
+                    ft.Text("Teste3")
+                ]
+            )
+        )
+
+        ###############################################################################
+        ###############################################################################
+
+        tela=ft.Container(
+            expand=True,
+            bgcolor="#D9D9D9",
+            content=ft.ResponsiveRow(
+                col={"xs":12,"sm":6,"md":4},
+                controls=[
+                    appbar,
+                    ft.Container(
+                        sidebar,
+                        alignment=ft.alignment.center_left #joga a sidebar para a esquerda, sem que estique pro restante da tela
+                    ),
+                ]
+            )
+        )
+
+        return tela
